183599d4c2b051ff66b100bcb3132b14ddd77c11
Paul Infield-Harm <paul@infield-harm.org>
2012-05-27 21:41:05 -0400

    Sampling

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,33 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(MyTableViewController.alloc.init)
+    @window.rootViewController.wantsFullScreenLayout = true
+    @window.makeKeyAndVisible
     true
   end
+end
+
+class MyTableViewController < UITableViewController
+  
+  def viewDidLoad
+    view.dataSource = self
+  end
+  
+  # Required method of UITableViewDataSource
+  def tableView(tableView, numberOfRowsInSection:section)
+    section == 0 ? 20 : 0
+  end
+  
+  CellID = 'Foo'
+  
+  # Required method of UITableViewDataSource
+  def tableView(tableView, cellForRowAtIndexPath:indexPath)
+    cell = tableView.dequeueReusableCellWithIdentifier(CellID) || begin
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:CellID)
+    end
+    cell.textLabel.text = "Cell #{indexPath.row}"
+    cell
+  end
+  
 end
