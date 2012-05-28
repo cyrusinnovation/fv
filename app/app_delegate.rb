f2991c65b764b0aab203c622877ad4528c941a06
Paul Infield-Harm <paul@infield-harm.org>
2012-05-27 22:17:01 -0400

    Custom regular cells

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -9,6 +9,7 @@ class AppDelegate
 end
 
 class MyTableViewController < UITableViewController
+  RegCount = 20
   
   def viewDidLoad
     view.dataSource = self
@@ -16,18 +17,61 @@ class MyTableViewController < UITableViewController
   
   # Required method of UITableViewDataSource
   def tableView(tableView, numberOfRowsInSection:section)
-    section == 0 ? 20 : 0
+    section == 0 ? RegCount + 1 : 0
   end
   
   CellID = 'Foo'
-  
+  AnotherCellID = 'Bar'
   # Required method of UITableViewDataSource
   def tableView(tableView, cellForRowAtIndexPath:indexPath)
-    cell = tableView.dequeueReusableCellWithIdentifier(CellID) || begin
-      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:CellID)
+    if indexPath.row < RegCount
+      cell = tableView.dequeueReusableCellWithIdentifier(CellID) || begin
+        RegularTableCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:CellID)
+      end
+      cell.text = "Cell #{indexPath.row}"
+      return cell
+    else
+      cell = tableView.dequeueReusableCellWithIdentifier(AnotherCellID) || begin
+        TextEntryTableCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:AnotherCellID)
+      end
+      return cell
     end
-    cell.textLabel.text = "Cell #{indexPath.row}"
-    cell
   end
   
 end
+
+class TextEntryTableCell < UITableViewCell
+  def initWithStyle(style, reuseIdentifier:reuseIdentifier)
+    if super
+      taskViewFrame = CGRectMake(0.0, 0.0, self.contentView.bounds.size.width, self.contentView.bounds.size.height)
+      textField = UITextField.alloc.initWithFrame(taskViewFrame)
+      textField.backgroundColor = UIColor.greenColor
+      textField.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight
+      self.contentView.addSubview(textField)
+    end
+    self
+  end
+  
+end
+
+
+class RegularTableCell < UITableViewCell
+  def initWithStyle(style, reuseIdentifier:reuseIdentifier)
+    if super
+      taskViewFrame = CGRectMake(0.0, 0.0, self.contentView.bounds.size.width, self.contentView.bounds.size.height)
+      taskView = UIView.alloc.initWithFrame(taskViewFrame)
+      taskView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight
+      @textLabel = UILabel.alloc.initWithFrame(taskViewFrame)
+      @textLabel.backgroundColor = UIColor.redColor
+      taskView.addSubview(@textLabel)
+      self.contentView.addSubview(taskView)
+    end
+    self
+  end
+  
+  def text=text
+    @textLabel.text = text
+  end
+  
+end
+
