455de72b7b1b2b149dfdb2ec33a95e44f5ea2c31
Paul Infield-Harm <paul@infield-harm.org>
2012-06-03 20:51:57 -0400

    Animation experimentation

--- app/list_view_controller.rb
+++ app/list_view_controller.rb
@@ -1,5 +1,5 @@
 class ListViewController < UIViewController
-  ItemHeight = 200
+  ItemHeight = 50
   
   def loadView
     self.view = UIScrollView.alloc.initWithFrame(UIScreen.mainScreen.bounds)
@@ -8,29 +8,42 @@ class ListViewController < UIViewController
     redView.backgroundColor = UIColor.redColor
     view.addSubview(redView)
 
-    blueView = UIView.alloc.initWithFrame(CGRectMake(0,ItemHeight,view.frame.size.width, ItemHeight))
-    blueView.backgroundColor = UIColor.blueColor
-    view.addSubview(blueView)
+    @blueView = UIView.alloc.initWithFrame(CGRectMake(0,ItemHeight,view.frame.size.width, ItemHeight))
+    @blueView.backgroundColor = UIColor.blueColor
+    view.addSubview(@blueView)
 
-    greenView = UIView.alloc.initWithFrame(CGRectMake(0,ItemHeight * 2,view.frame.size.width, ItemHeight))
-    greenView.backgroundColor = UIColor.greenColor
+    @greenView = UIView.alloc.initWithFrame(CGRectMake(0,ItemHeight * 2,view.frame.size.width, ItemHeight))
+    @greenView.backgroundColor = UIColor.greenColor
     
     recognizer = UITapGestureRecognizer.alloc.initWithTarget(self, action:"greenTapped")
     
-    greenView.addGestureRecognizer(recognizer)
-    view.addSubview(greenView)
+    @greenView.addGestureRecognizer(recognizer)
+    view.addSubview(@greenView)
     
 
-    yellowView = UIView.alloc.initWithFrame(CGRectMake(0,ItemHeight * 3,view.frame.size.width, ItemHeight))
-    yellowView.backgroundColor = UIColor.yellowColor
-    view.addSubview(yellowView)
-
+    @yellowView = UIView.alloc.initWithFrame(CGRectMake(0,ItemHeight * 3,view.frame.size.width, ItemHeight))
+    @yellowView.backgroundColor = UIColor.yellowColor
+    view.addSubview(@yellowView)
+    
     
     view.contentSize = CGSizeMake(view.frame.size.width, ItemHeight * 4);
   end
   
   def greenTapped
-    puts "foo"
+    UIView.beginAnimations('animationID', context:nil)
+    
+    newBlueFrame = CGRectMake(0,ItemHeight,view.frame.size.width, ItemHeight * 2)
+    @blueView.frame = newBlueFrame
+
+    newGreenFrame = CGRectMake(0,ItemHeight * 3,view.frame.size.width, ItemHeight * 2)
+    @greenView.frame = newGreenFrame
+    
+    newYellowFrame = CGRectMake(0, ItemHeight * 5, view.frame.size.width, ItemHeight)
+    @yellowView.frame = newYellowFrame
+    
+    newContentSize = CGSizeMake(view.frame.size.width, ItemHeight * 6)
+    view.contentSize = newContentSize
+    UIView.commitAnimations
   end
   
   def viewDidLoad
