4e95b7792f00d613a679f5fa940bd81602253849
Paul Infield-Harm <paul@infield-harm.org>
2012-06-05 21:40:28 -0400

    Adding text box

--- app/task_view_controller.rb
+++ app/task_view_controller.rb
@@ -1,15 +1,15 @@
 class TaskViewController < UIViewController
   TaskHeight = 50
+  TextEntryHeight = 50
   
   def loadView
+    self.view = UIView.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @scrollView = UIScrollView.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    self.view = @scrollView
-    
-    scrollPadding = @scrollView.frame.size.height - TaskHeight
-    
-    view.contentSize = CGSizeMake(view.frame.size.width, (TaskStore.shared.tasks.size * TaskHeight) + scrollPadding)
-    view.delegate = self
+    view.addSubview(@scrollView)
+    @scrollView.contentSize = CGSizeMake(@scrollView.frame.size.width, TaskStore.shared.tasks.size * TaskHeight)
+    @scrollView.delegate = self
     drawTasks
+    # drawTextBox
   end
   
   def scrollViewDidScroll(scrollView)
@@ -32,19 +32,28 @@ class TaskViewController < UIViewController
       task = TaskStore.shared.tasks[index]
       subview = task_view(index, task)
       @task_views << subview
-      view.addSubview(subview)
+      @scrollView.addSubview(subview)
       if task.dotted?
         @selected_indexes << index
       end
     end
     
     @selected_indexes.each do |index|
-      view.bringSubviewToFront(@task_views[index])
+      @scrollView.bringSubviewToFront(@task_views[index])
     end
   end
   
+  # def drawTextBox
+  #   # draw text box on bottom of scrollview
+  #   text_box_frame = CGRectMake(0, @task_views.size * TaskHeight, view.frame.size.width, TaskHeight)
+  #   @text_box = UITextField.alloc.initWithFrame(text_box_frame)
+  #   @text_box.delegate = self
+  #   @text_box.backgroundColor = UIColor.redColor
+  #   view.addSubview(@text_box)
+  # end
+  
   def task_view(index, task)
-    task_frame = CGRectMake(0, TaskHeight * index, view.frame.size.width, TaskHeight)
+    task_frame = CGRectMake(0, TaskHeight * index, @scrollView.frame.size.width, TaskHeight)
     task_view = UIView.alloc.initWithFrame(task_frame)
     task_label = UILabel.alloc.initWithFrame(CGRectMake(0,0, task_view.frame.size.width, task_view.frame.size.height))
     task_label.text = task.text
@@ -53,6 +62,18 @@ class TaskViewController < UIViewController
       task_label.backgroundColor = UIColor.grayColor
     end
     task_view
+  end
+  
+  # Defined for UITextFieldDelegate
+  def textFieldShouldReturn(textField)
+    textField.resignFirstResponder
+    textField.text = nil
+    true
+  end
+  
+  def textFieldDidEndEditing(textField)
+    puts textField.text
+    true
   end
 
   
