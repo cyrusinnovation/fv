7dfd101b217d7d9be15a725c4217e50e6754012f
Paul Infield-Harm <paul@infield-harm.org>
2012-06-14 10:40:28 -0400

    Added dropbox button

--- app/dropbox_view_controller.rb
+++
@@ -1,6 +0,0 @@
-class DropboxViewController < UIViewController
-  def loadView
-    view = UIView.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
-    view.backgroundColor = UIColor.redColor
-  end
-end
--- app/export_view_controller.rb
+++ app/export_view_controller.rb
@@ -1,6 +1,29 @@
 class ExportViewController < UIViewController
   def loadView
-    view = UIView.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
-    view.backgroundColor = UIColor.redColor
+    self.view = UIView.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
+    
+
+    button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    button.frame = CGRectMake(10,10,view.frame.size.width - 20,50)
+    button.addTarget(self, action:'buttonClicked', forControlEvents:UIControlEventTouchUpInside)
+    button.setTitle("Export to Dropbox",forState:UIControlStateNormal)
+    
+    view.addSubview(button)    
   end
+  
+  def buttonClicked
+    
+    puts "click!"
+    
+    # 
+    # DBSession.sharedSession.linkFromController(self) unless DBSession.sharedSession.isLinked
+    # 
+    # writeToTextFile
+    
+    # localPath = NSBundle.mainBundle.pathForResource("Info", ofType:"plist");
+    # filename = "Info.plist";
+    # destDir = "/";
+    # restClient.uploadFile(filename, toPath:destDir, withParentRev:nil, fromPath:localPath);
+  end
+  
 end
