78eaf5b24f04bb517d0d242ac7289f44da741623
Paul Infield-Harm <paul@infield-harm.org>
2012-06-26 11:51:50 -0400

    Pull some controller code into subclass of scrollview

--- app/task_view_controller.rb
+++ app/task_view_controller.rb
@@ -1,7 +1,6 @@
 class TaskViewController < UIViewController
   include Notifications
   
-  TaskHeight = 40
   TextEntryHeight = 40
   
   def initWithStore(task_store)
@@ -15,7 +14,7 @@ class TaskViewController < UIViewController
     self.view = UIView.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
     
     scroll_view_frame = CGRectMake(0, 0, view.frame.size.width, view.frame.size.height)
-    @scroll_view = UIScrollView.alloc.initWithFrame(scroll_view_frame)
+    @scroll_view = TaskListView.alloc.initWithFrame(scroll_view_frame, taskStore:@task_store)
     view.addSubview(@scroll_view)
     @scroll_view.delegate = self
 
@@ -34,7 +33,7 @@ class TaskViewController < UIViewController
     observe(AddTappedNotification, action:'handleAddTapped')
     observe(UIKeyboardDidShowNotification, action:'handleKeyboardDidShow')
     
-    drawTasks
+    @scroll_view.drawTasks
   end
 
   def handleAddTapped(notification)
@@ -74,115 +73,14 @@ class TaskViewController < UIViewController
   end
   
   def redraw_tasks
-    @scroll_view.subviews.each do |task_view|
-      task_view.removeFromSuperview
-    end
-    drawTasks
+    @scroll_view.redraw_tasks
   end
   
   def scrollViewDidScroll(scrollView)
     # As currently designed, this is a display memory hog.
     # Here is where we would allocate or deallocate views based on the contentOffset
-    adjust_selected
+    @scroll_view.adjust_selected
   end 
-
-  def adjust_selected
-    yoffset = @scroll_view.contentOffset.y
-    
-    @selected_indexes_for_adjust.each do |index|
-      y = y_for_view(index, @selected_indexes_for_adjust, yoffset)
-      @task_views_for_adjust[index].frame = CGRectMake(0,y,@scroll_view.frame.size.width,TaskHeight)
-    end    
-  end
-  
-  def drawTasks
-
-    selected_indexes = []
-    @task_store.tasks.each_index do |index|
-      task = @task_store.tasks[index]
-      if task.dotted?
-        selected_indexes << index
-      end
-    end
-
-    yoffset = @scroll_view.contentOffset.y
-
-    task_views = []
-    @task_store.tasks.each_index do |index|
-      task = @task_store.tasks[index]
-      y = y_for_view(index, selected_indexes, yoffset)
-      task_frame = CGRectMake(0, y, @scroll_view.frame.size.width, TaskHeight)
-      subview = TaskView.alloc.initWithFrame(task_frame, task:task, position:index)
-      task_views << subview
-      @scroll_view.addSubview(subview)
-    end
-    
-    selected_indexes.each do |index|
-      @scroll_view.bringSubviewToFront(task_views[index])
-    end
-    
-    @scroll_view.contentSize = CGSizeMake(@scroll_view.frame.size.width, @task_store.tasks.size * TaskHeight)
-    
-    collect_adjust_data
-
-  end
-
-  def y_for_view(index, selected_indexes, yoffset)
-    
-    # This is the position of the indexth view would
-    # take if there wasn't any stickiness
-    normal_pos = TaskHeight * index
-    
-    # position of index within list of selected indexes
-    # nil if not present
-    selected_index = selected_indexes.index(index)
-    
-    # If the selected_indexes doesn't contain index, then 
-    # that view isn't one of a selected one.
-    return normal_pos unless selected_index
-
-    # If the normal position is far enough down then just put it in
-    # its normal spot.
-    return normal_pos if normal_pos > yoffset
-
-    # Get the index into the list of views for the next selected view
-    # if it exists
-    next_selected = selected_indexes[selected_index + 1]
-
-    if next_selected
-      next_normal_pos = next_selected * TaskHeight
-      
-      # If the next selected is bumping into this one, get out of the way
-      if next_normal_pos > yoffset && next_normal_pos < yoffset + TaskHeight
-        return next_normal_pos - TaskHeight
-      else
-        return yoffset
-      end
-    else
-      if yoffset > normal_pos 
-        return yoffset
-      else
-        return normal_pos
-      end
-    end
-  end
-
-
-  def collect_adjust_data
-    @task_views_for_adjust = []
-    @scroll_view.subviews.each do |subview|
-      @task_views_for_adjust[subview.position] = subview
-    end
-
-    @selected_indexes_for_adjust = []
-    @task_store.tasks.each_index do |index|
-      task = @task_store.tasks[index]
-      if task.dotted?
-        @selected_indexes_for_adjust << index
-      end
-    end
-  end
-  
   
   # presenter method
   def show_task_input
---
+++ app/views/task_list_view.rb
@@ -0,0 +1,118 @@
+class TaskListView < UIScrollView
+  TaskHeight = 40
+
+  def initWithFrame(frame, taskStore:task_store)
+    if initWithFrame(frame)
+      @task_store = task_store
+      NSLog("Task_store: #{@task_store}")
+    end
+    self
+  end
+
+
+  def drawTasks
+
+    selected_indexes = []
+    @task_store.tasks.each_index do |index|
+      task = @task_store.tasks[index]
+      if task.dotted?
+        selected_indexes << index
+      end
+    end
+
+    yoffset = self.contentOffset.y
+
+    task_views = []
+    @task_store.tasks.each_index do |index|
+      task = @task_store.tasks[index]
+      y = y_for_view(index, selected_indexes, yoffset)
+      task_frame = CGRectMake(0, y, self.frame.size.width, TaskHeight)
+      subview = TaskView.alloc.initWithFrame(task_frame, task:task, position:index)
+      task_views << subview
+      self.addSubview(subview)
+    end
+    
+    selected_indexes.each do |index|
+      self.bringSubviewToFront(task_views[index])
+    end
+    
+    self.contentSize = CGSizeMake(self.frame.size.width, @task_store.tasks.size * TaskHeight)
+    
+    collect_adjust_data
+
+  end
+  
+  def y_for_view(index, selected_indexes, yoffset)
+    
+    # This is the position of the indexth view would
+    # take if there wasn't any stickiness
+    normal_pos = TaskHeight * index
+    
+    # position of index within list of selected indexes
+    # nil if not present
+    selected_index = selected_indexes.index(index)
+    
+    # If the selected_indexes doesn't contain index, then 
+    # that view isn't one of a selected one.
+    return normal_pos unless selected_index
+
+    # If the normal position is far enough down then just put it in
+    # its normal spot.
+    return normal_pos if normal_pos > yoffset
+
+    # Get the index into the list of views for the next selected view
+    # if it exists
+    next_selected = selected_indexes[selected_index + 1]
+
+    if next_selected
+      next_normal_pos = next_selected * TaskHeight
+      
+      # If the next selected is bumping into this one, get out of the way
+      if next_normal_pos > yoffset && next_normal_pos < yoffset + TaskHeight
+        return next_normal_pos - TaskHeight
+      else
+        return yoffset
+      end
+    else
+      if yoffset > normal_pos 
+        return yoffset
+      else
+        return normal_pos
+      end
+    end
+  end
+
+
+  def collect_adjust_data
+    @task_views_for_adjust = []
+    self.subviews.each do |subview|
+      @task_views_for_adjust[subview.position] = subview
+    end
+
+    @selected_indexes_for_adjust = []
+    @task_store.tasks.each_index do |index|
+      task = @task_store.tasks[index]
+      if task.dotted?
+        @selected_indexes_for_adjust << index
+      end
+    end
+  end
+  
+  def adjust_selected
+    yoffset = self.contentOffset.y
+    
+    @selected_indexes_for_adjust.each do |index|
+      y = y_for_view(index, @selected_indexes_for_adjust, yoffset)
+      @task_views_for_adjust[index].frame = CGRectMake(0,y,self.frame.size.width,TaskHeight)
+    end    
+  end
+  
+  def redraw_tasks
+    self.subviews.each do |task_view|
+      task_view.removeFromSuperview
+    end
+    self.drawTasks
+  end
+  
+  
+end
