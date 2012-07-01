df7caca7643c53699275346e7bcf5bcb55968ef2
Paul Infield-Harm <paul@infield-harm.org>
2012-07-01 10:48:34 -0400

    Revert pink button

--- app/notifications.rb
+++ app/notifications.rb
@@ -13,8 +13,6 @@ module Notifications
   TaskViewRightSwipeNotification = 'TaskViewRightSwipe'
   TaskViewLeftSwipeNotification = 'TaskViewLeftSwipe'
 
-  ActionButtonTappedNotification = 'ActionButtonTapped'
-  
   def observe(notificationName, action:handler)
     NSNotificationCenter.defaultCenter.addObserver(self, selector:"#{handler}:", name:notificationName, object:nil)
   end
--- app/views/task_view.rb
+++ app/views/task_view.rb
@@ -21,16 +21,7 @@ class TaskView < UIView
       leftRecognizer = UISwipeGestureRecognizer.alloc.initWithTarget(self, action:"handleLeftSwipe")
       leftRecognizer.direction = UISwipeGestureRecognizerDirectionLeft
       addGestureRecognizer(leftRecognizer)
-      @label.backgroundColor = self.backgroundColor = UIColor.grayColor
-      
-      action_button = ButtonView.alloc.initWithImageNamed("pink_button.png", tapNotification:ActionButtonTappedNotification)
-      action_button_frame = CGRectMake(
-        self.frame.size.width - action_button.frame.size.width - 10, 
-        self.frame.size.height - action_button.frame.size.height - 10, 
-        action_button.frame.size.width, 
-        action_button.frame.size.height)
-      action_button.frame = action_button_frame
-      addSubview(action_button)
+      @label.backgroundColor = self.backgroundColor = UIColor.redColor
     elsif task.dotted?
       @label.backgroundColor = self.backgroundColor = UIColor.grayColor
     else
