da272bb44394742540115fb152501c52f9bbcfc3
Paul Infield-Harm <paul@infield-harm.org>
2012-07-03 00:39:51 -0400

    Top updates with model changes

--- app/controllers/top_view_controller.rb
+++ app/controllers/top_view_controller.rb
@@ -4,16 +4,13 @@ class TopViewController < UIViewController
   def initWithStore(task_store)
     if init
       @task_store = task_store
-
-      @tasks = @task_store.tasks
-      @selected_indexes = []
-      @tasks.each_index do |index|
-        task = @tasks[index]
-        if task.dotted?
-          @selected_indexes << index
-        end
-      end
-      @selected_indexes.reverse!
+      update_indexes
+      @last_y_offset = 0
+      # Observe model changes.
+      observe(TaskAddedNotification, action:'handleModelChange')
+      observe(TaskChangedNotification, action:'handleModelChange')
+      observe(TaskRemovedNotification, action:'handleModelChange')
+      observe(TaskPausedNotification, action:'handleModelChange')
     end
     self
   end
@@ -24,10 +21,24 @@ class TopViewController < UIViewController
     @task_view.hidden = true
     view.addSubview(@task_view)
     observe(ScrollViewMovedNotification, action:'handleScrollViewMoved')
+    update_for_yoffset(@last_y_offset)
   end
 
   def handleScrollViewMoved(notification)
     y_offset = notification.object.view.contentOffset.y
+    update_for_yoffset(y_offset)
+  end
+  
+  def handleModelChange(notification)
+    update_indexes
+    update_for_yoffset(@last_y_offset)
+  end
+
+  private
+  
+  def update_for_yoffset(y_offset)
+    @last_y_offset = y_offset
+    
     logical_y = (y_offset / TaskListView::TaskHeight).floor
     show_this = @selected_indexes.find { |index| index <= logical_y }
     
@@ -39,6 +50,18 @@ class TopViewController < UIViewController
       @task_view.hidden = false
     end
   end
-
+  
+  
+  def update_indexes
+    @tasks = @task_store.tasks
+    @selected_indexes = []
+    @tasks.each_index do |index|
+      task = @tasks[index]
+      if task.dotted?
+        @selected_indexes << index
+      end
+    end
+    @selected_indexes.reverse!
+  end
 
 end
